38905e844f6cceb9e98c777e0a11e14f9102db29
vpadillap <158070557+vpadillap@users.noreply.github.com>
2024-01-31 10:36:45 -0500

    Working with hooks to optimize code

--- TestCafe/tests/Session1/tests.js
+++ TestCafe/tests/Session1/tests.js
@@ -1,7 +1,21 @@
 import {Selector} from  "testcafe";
 
-fixture('Getting Started')
-    .page('https://appv2.vocana.co/');
+fixture ('Tests Vocana')
+    .page ('https://appv2.vocana.co/')
+    
+    //Hook to reuse resize and login code for each test
+    .beforeEach ( async t => {
+
+        const email = Selector("#Email");
+        const password = Selector("#Password");
+        const botonLogIn = Selector('.btn.btn-principal-pseudo-classes.btn-purple-57.btn-large.w-full');
+    
+        await t
+            .resizeWindow(1600, 700)
+            .typeText(email, 'jchung@sparxworks.com')
+            .typeText(password, 'Aa.123456')
+            .click(botonLogIn);
+    });
 
     //Create a date string to help us validate the tests results with 'expect' method
     const dateToday = new Date();
@@ -10,39 +24,20 @@ fixture('Getting Started')
     const year = dateToday.getFullYear();
     const fullDate = [day, month, year].join('/');
 
+    //Webpage common elements
+    const profile = Selector('span').withText('Profile');
+    const createPost = Selector('span').withText('Create Post');
+    const textareaElement = Selector('textarea.mb-4.h-40.w-full.rounded.border-b.border-turquoise.bg-cobalt-22.pl-1.focus\\:border-turquoise');
 
+    //Template begins here
 
-    //Test name as a variable, template begins here
-
-    let testName = "Profile new post no img";
+     test( "Profile new post no img", async t => {
     
-    test( testName, async t => {
-    
-        let datedTestText = testName + " " + fullDate;
-
-        //First, we resize the window to have the desired view of the website
-
-        await
-
-            t.resizeWindow(1600, 700);
-
-        
-        //Login
-
-        const email = Selector("#Email");
-        const password = Selector("#Password");
-        const botonLogIn = Selector('.btn.btn-principal-pseudo-classes.btn-purple-57.btn-large.w-full');
-        const profile = Selector('span').withText('Profile');
-        const createPost = Selector('span').withText('Create Post');
-        const textareaElement = Selector('textarea.mb-4.h-40.w-full.rounded.border-b.border-turquoise.bg-cobalt-22.pl-1.focus\\:border-turquoise');
-    
-        await t
-            .typeText(email, 'jchung@sparxworks.com')
-            .typeText(password, 'Aa.123456')
-            .click(botonLogIn)
+        let datedTestText = t.testRun.test.name + " " + fullDate;
 
         //Actual test begins
 
+        await t
             .click(profile)
             .click(createPost)  
             .typeText(textareaElement, datedTestText);
@@ -52,41 +47,22 @@ fixture('Getting Started')
         await t
             .click(publishPost);
 
+            console.log(datedTestText);
         //Validate
         await t
             .expect(Selector('span').withText(datedTestText).exists).ok();
 
     });
 
-    testName = "Profile new post with img";
-    
-    test( testName, async t => {
+
+  
+    test( "Profile new post with img", async t => {
         
-        let datedTestText = testName + " " + fullDate;
-
-        //First, we resize the window to have the desired view of the website
-
-        await
-
-            t.resizeWindow(1600, 700);
-
-        
-        //Login
-
-        const email = Selector("#Email");
-        const password = Selector("#Password");
-        const botonLogIn = Selector('.btn.btn-principal-pseudo-classes.btn-purple-57.btn-large.w-full');
-        const profile = Selector('span').withText('Profile');
-        const createPost = Selector('span').withText('Create Post');
-        const textareaElement = Selector('textarea.mb-4.h-40.w-full.rounded.border-b.border-turquoise.bg-cobalt-22.pl-1.focus\\:border-turquoise');
-    
-        await t
-            .typeText(email, 'jchung@sparxworks.com')
-            .typeText(password, 'Aa.123456')
-            .click(botonLogIn)
+        let datedTestText = t.testRun.test.name + " " + fullDate;
 
         //Actual test begins
 
+        await t
             .click(profile)
             .click(createPost)  
             .typeText(textareaElement, datedTestText);
