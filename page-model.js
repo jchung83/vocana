e2c15276329d9428b1f01ca1e4f355a1657f8b57
vpadillap <158070557+vpadillap@users.noreply.github.com>
2024-03-19 14:51:19 -0500

    Refactor update

--- page-model.js
+++ page-model.js
@@ -13,22 +13,35 @@ export class User {
 }
 
 export const admin = new User('admin@sparxworks.com', 'Admin#2024');
-export const artist = new User('art@art.com', 'Aa.123456' );
+export const artist = new User('artist@artist.com', 'Aa.123456' );
 export const fan = new User('fan@fan.com','Aa.123456');
 export const fake =new User('fake@fake.com', 'notworking');
-
-/*
-* Login page
-*/
 
 export class LoginPage {
     constructor () {
         this.emailInput = Selector('#Email');
         this.passInput = Selector('#Password');
-        this.rememberMe = label.withText("Remember me");
+        this.rememberMe = Selector('label[for="rememberMe"]');
         this.rememberMeCheck = Selector('input[id="rememberMe"]');
         this.loginButton = span.withText("Log In");
         this.errorLogin = span.withText("Invalid email or password.");
+    }
+    async login (username) {
+        await t
+        .typeText(this.emailInput, username.email)
+        .typeText(this.passInput, username.password)
+        .click(this.loginButton)
+    }
+}
+
+export class AdminLoginPage {
+    constructor () {
+        this.emailInput = Selector('#email');
+        this.passInput = Selector('#password');
+        this.rememberMe = Selector('label[for="rememberMe"]');
+        this.rememberMeCheck = Selector('input[id="rememberMe"]');
+        this.loginButton = span.withText("Log In");
+        this.errorLogin = span.withText("errorInvalidEmailPassword");
     }
     async login (username) {
         await t
@@ -44,16 +57,78 @@ export class HomePage {
         this.menuHome = Selector('a[aria-label="Navigate to Home"]');
         this.menuMusic = Selector('a[aria-label="Navigate to Music"]');
         this.menuMerch = Selector('a[aria-label="Navigate to Merch"]');
-        this.liveStream = Selector('a[aria-label="Navigate to Livestream"]');
-        this.liveStream = Selector('a[aria-label="Navigate to Search"]');
+        this.menuStream = Selector('a[aria-label="Navigate to Livestream"]');
+        this.menuSearch = Selector('a[aria-label="Navigate to Search"]');
 
-        this.liveStream = Selector('a[aria-label="Navigate to Notifications"]');
-        this.liveStream = Selector('a[aria-label="Navigate to Messages"]');
+        this.menuNotifications = Selector('a[aria-label="Navigate to Notifications"]');
+        this.menuMessages = Selector('a[aria-label="Navigate to Messages"]');
 
-        this.liveStream = Selector('a[aria-label="Navigate to Concerts"]');
-        this.liveStream = Selector('a[aria-label="Navigate to Audience"]');
-        this.liveStream = Selector('a[aria-label="Navigate to Wallet"]');
-        this.liveStream = Selector('a[aria-label="Navigate to Settings"]');
-        this.liveStream = Selector('button[aria-label="Log Out"]');    
+        this.menuConcerts = Selector('a[aria-label="Navigate to Concerts"]');
+        this.menuAudience = Selector('a[aria-label="Navigate to Audience"]');
+        this.menuWallet = Selector('a[aria-label="Navigate to Wallet"]');
+        this.menuSettings = Selector('a[aria-label="Navigate to Settings"]');
+        this.menuLogout = Selector('button[aria-label="Log Out"]');
+
+    }
+    async checkmenuartist () {
+        await t
+        .expect(this.menuProfile.exists).ok()
+        .expect(this.menuHome.exists).ok()
+        .expect(this.menuMusic.exists).ok()
+        .expect(this.menuMerch.exists).ok()
+        .expect(this.menuStream.exists).ok()
+        .expect(this.menuSearch.exists).ok()
+
+        .expect(this.menuNotifications.exists).ok()
+        .expect(this.menuMessages.exists).ok()
+
+        .expect(this.menuConcerts.exists).ok()
+        .expect(this.menuAudience.exists).ok()
+        .expect(this.menuWallet.exists).ok()
+        .expect(this.menuSettings.exists).ok()
+        .expect(this.menuLogout.exists).ok()
+    }
+    async checkmenufan () {
+        await t
+        .expect(this.menuProfile.exists).ok()
+        .expect(this.menuHome.exists).ok()
+        .expect(this.menuMusic.exists).ok()
+        .expect(this.menuMerch.exists).ok()
+        .expect(this.menuStream.exists).ok()
+        .expect(this.menuSearch.exists).ok()
+
+        .expect(this.menuNotifications.exists).ok()
+        .expect(this.menuMessages.exists).ok()
+
+        .expect(this.menuConcerts.exists).notOk()
+        .expect(this.menuAudience.exists).ok()
+        .expect(this.menuWallet.exists).ok()
+        .expect(this.menuSettings.exists).ok()
+        .expect(this.menuLogout.exists).ok()
+    }
+}
+
+export class AdminHomePage {
+    constructor () {
+        this.menuProfile = Selector('a[aria-label="Navigate to Profile"]');
+
+        this.menuHome = Selector('a[aria-label="Navigate to Home"]');
+        this.menuInternalManager = Selector('a[aria-label="Navigate to Internal Manager"]');
+        this.menuUserManager = Selector('a[aria-label="Navigate to User Manager"]');
+        this.menuSupportManager = Selector('a[aria-label="Navigate to Support Manager"]');
+        this.menuReconciliation = Selector('a[aria-label="Navigate to Reconciliation"]');
+
+        this.menuLogout = Selector('button[aria-label="Log Out"]');
+
+    }
+    async checkmenuadmin () {
+        await t
+        .expect(this.menuProfile.exists).ok()
+        .expect(this.menuInternalManager.exists).ok()
+        .expect(this.menuUserManager.exists).ok()
+        .expect(this.menuSupportManager.exists).ok()
+        .expect(this.menuReconciliation.exists).ok()
+
+        .expect(this.menuLogout.exists).ok()
     }
 }
--- vocana-tests.js
+++ vocana-tests.js
@@ -1,9 +1,11 @@
 import { Selector } from 'testcafe';
-import { LoginPage, User } from './page-model'; // Import User separately
-import { admin } from './page-model';
+import { LoginPage, User, HomePage, AdminLoginPage, AdminHomePage } from './page-model'; // Import User separately
+import { admin, artist, fan, fake } from './page-model';
 
 const loginPage = new LoginPage();
-
+const homePage = new HomePage();
+const adminLoginPage = new AdminLoginPage();
+const adminHomePage = new AdminHomePage();
 
 //Create a date string to help us validate the tests results with 'expect' method
 const dateToday = new Date();
@@ -19,8 +21,51 @@ const fullDate = [DMY, time].join('-');
 
 fixture ('Login Page')
     .page ('https://appv2.vocana.co/')
+    .beforeEach ( async t => {
+    await t.resizeWindow(1600, 720)
+    });
     
-    test( "Login Admin", async t => {
-        await
-        loginPage.login(admin);
+    test( "Artist login and Main menu", async t => {
+        await loginPage.login(artist);
+        await homePage.checkmenuartist();
+    });
+
+    test( "Fan login and Main menu", async t => {
+        await loginPage.login(fan);
+        await homePage.checkmenufan();
+    });
+
+    test( "Failed login attempt", async t => {
+        await loginPage.login(fake);
+        await t
+        .expect(loginPage.errorLogin.exists).ok()
+    });
+
+    test( "Remember Me Checkbox", async t => {
+        await t
+            .click(loginPage.rememberMe)
+            .expect(loginPage.rememberMeCheck.checked).ok()
+    });
+
+fixture ('Admin Login Page')
+    .page ('https://adminv2.vocana.co/')
+    .beforeEach ( async t => {
+        await t.resizeWindow(1600, 720)
+        });
+
+    test( "Admin login and Main menu", async t => {
+        await adminLoginPage.login(admin);
+        await adminHomePage.checkmenuadmin();
+    });
+
+    test( "Admin failed login attempt", async t => {
+        await adminLoginPage.login(fake);
+        await t
+        .expect(adminLoginPage.errorLogin.exists).ok()
+    });
+
+    test( "Admin Remember Me Checkbox", async t => {
+        await t
+            .click(adminLoginPage.rememberMe)
+            .expect(adminLoginPage.rememberMeCheck.checked).ok()
     });
